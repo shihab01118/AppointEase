8dc080145c6728f627073fa7c2ae6ef0ee92a37f
shihab01118 <shahed.shihab.0@gmail.com>
2024-12-03 15:42:28 +0600

    sidebar added with navigation links in dashboard layout

---
+++ app/components/DashboardLinks.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import {
+  CalendarCheck,
+  HomeIcon,
+  LucideProps,
+  Settings,
+  Users2,
+} from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ForwardRefExoticComponent, RefAttributes } from "react";
+
+interface INavigationProps {
+  id: number;
+  name: string;
+  href: string;
+  icon: ForwardRefExoticComponent<
+    Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
+  >;
+}
+
+export const dashboardLinks: INavigationProps[] = [
+  {
+    id: 1,
+    name: "Event Types",
+    href: "/dashboard",
+    icon: HomeIcon,
+  },
+  {
+    id: 2,
+    name: "Meetings",
+    href: "/dashboard/meetings",
+    icon: Users2,
+  },
+  {
+    id: 3,
+    name: "Availability",
+    href: "/dashboard/availability",
+    icon: CalendarCheck,
+  },
+  {
+    id: 4,
+    name: "Settings",
+    href: "/dashboard/settings",
+    icon: Settings,
+  },
+];
+
+export function DashboardLinks() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {dashboardLinks.map((link) => (
+        <Link
+          key={link.id}
+          href={link.href}
+          className={cn(
+            pathname === link.href
+              ? "text-primary bg-primary/10"
+              : "text-muted-foreground hover:text-foreground",
+            "flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary"
+          )}
+        >
+          <link.icon className="size-4" />
+          {link.name}
+        </Link>
+      ))}
+    </>
+  );
+}
---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ReactNode } from "react";
+
+import Logo from "@/public/logo.png";
+import { DashboardLinks } from "../components/DashboardLinks";
+
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <div className="min-h-screen w-full grid md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+        <div className="hidden md:block border-r bg-muted/40">
+          <div className="flex h-full max-h-screen flex-col gap-2">
+            <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+              <Link href="/" className="flex items-center gap-2">
+                <Image src={Logo} alt="Logo" className="size-8" />
+                <p className="text-xl font-bold">
+                  Appoint<span className="text-primary">Ease</span>
+                </p>
+              </Link>
+            </div>
+
+            <div className="flex-1">
+              <nav className="grid items-start px-2 lg:px-4">
+                <DashboardLinks />
+              </nav>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col">
+          <header className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6 bg-muted/40"></header>
+
+          <main>{children}</main>
+        </div>
+      </div>
+    </>
+  );
+}
