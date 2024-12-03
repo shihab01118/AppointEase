18230e612bd2bee3ca6db09b75c3f6a1ce0f959a
shihab01118 <shahed.shihab.0@gmail.com>
2024-12-03 17:05:57 +0600

    header added at dashboard layout with theme toggle feature and user button

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,32 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import Link from "next/link";
 import { ReactNode } from "react";
 
 import Logo from "@/public/logo.png";
 import { DashboardLinks } from "../components/DashboardLinks";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import { ThemeToggle } from "../components/ThemeToggle";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { signOut } from "../lib/auth";
+import { requireUser } from "../lib/hooks";
 
-export default function DashboardLayout({ children }: { children: ReactNode }) {
+export default async function DashboardLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const session = await requireUser();
+
   return (
     <>
       <div className="min-h-screen w-full grid md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
@@ -21,7 +42,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
             </div>
 
             <div className="flex-1">
-              <nav className="grid items-start px-2 lg:px-4">
+              <nav className="grid items-start px-4 lg:px-6">
                 <DashboardLinks />
               </nav>
             </div>
@@ -29,7 +50,66 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
         </div>
 
         <div className="flex flex-col">
-          <header className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6 bg-muted/40"></header>
+          <header className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6 bg-muted/40">
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button
+                  className="md:hidden shrink-0"
+                  size="icon"
+                  variant="outline"
+                >
+                  <Menu className="size-5" />
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="left" className="flex flex-col">
+                <nav className="grid gap-2 mt-10">
+                  <DashboardLinks />
+                </nav>
+              </SheetContent>
+            </Sheet>
+
+            <div className="ml-auto flex items-center gap-x-4">
+              <ThemeToggle />
+
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="secondary"
+                    size="icon"
+                    className="rounded-full"
+                  >
+                    <img
+                      src={session?.user?.image as string}
+                      alt="Profile Image"
+                      width={20}
+                      height={20}
+                      className="w-full h-full rounded-full"
+                    />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem asChild>
+                    <Link href="/dashboard/settings">Settings</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <DropdownMenuItem asChild>
+                      <form
+                        className="w-full"
+                        action={async () => {
+                          "use server";
+                          await signOut();
+                        }}
+                      >
+                        <button className="w-full text-left">Log Out</button>
+                      </form>
+                    </DropdownMenuItem>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </header>
 
           <main>{children}</main>
         </div>
