3c022ca4e07a086ca62ad5f3650c913e592c2089
shihab01118 <shahed.shihab.0@gmail.com>
2024-12-03 14:19:36 +0600

    shade cn added, navbar added at home page, google and github authentication added with next-auth

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+
+import { requireUser } from "../lib/hooks";
+
+export default async function DashboardPage() {
+  const session = await requireUser();
+
+  return (
+    <div>
+      <h1>Hello from DashboardPage</h1>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import { redirect } from "next/navigation";
+import { Navbar } from "./components/Navbar";
+import { auth } from "./lib/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
+  if (session?.user) {
+    return redirect("/dashboard");
+  }
+
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <Navbar />
     </div>
   );
 }
