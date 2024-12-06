d44e902b35aa5fa3826333f7f5b402aaa276ba3c
shihab01118 <shahed.shihab.0@gmail.com>
2024-12-06 22:11:07 +0600

    testimonial and cta section added

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -32,7 +32,7 @@ export function Features() {
     <div className="py-24 ">
       <div className="max-w-2xl mx-auto lg:text-center">
         <p className="font-semibold leading-7 text-primary">Schedule faster</p>
-        <h1 className="mt-2 text-3xl font-semibold tracking-tight sm:text-4xl">
+        <h1 className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
           Schedule meetings in minutes!
         </h1>
         <p className="mt-6 text-base leading-snug text-muted-foreground">
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { auth } from "./lib/auth";
 import { Hero } from "./components/Hero";
 import { Logos } from "./components/Logos";
 import { Features } from "./components/Features";
+import { Testimonial } from "./components/Testimonial";
+import { CTA } from "./components/Cta";
 
 export default async function Home() {
   const session = await auth();
@@ -18,6 +20,8 @@ export default async function Home() {
       <Hero />
       <Logos />
       <Features />
+      <Testimonial />
+      <CTA />
     </div>
   );
 }
